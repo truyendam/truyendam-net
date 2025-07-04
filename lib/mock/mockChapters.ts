b26d1dc517be9b68b9448e0cac893fd2faa65f02
moonlust-studio <thegioigiay360jp@gmail.com>
2025-07-05 02:04:11 +0900

    ✨ Cập nhật banner trang chủ Moonlust

--- lib/mock/mockChapters.ts
+++ lib/mock/mockChapters.ts
@@ -81,6 +81,46 @@ const mockChapters: Record<
       id: 5,
       title: "Chương 5",
       hasMarkdown: true
+    },
+     6: {
+      id: 6,
+      title: "Chương 6",
+      hasMarkdown: true
+    },
+     7: {
+      id: 7,
+      title: "Chương 7",
+      hasMarkdown: true
+    },
+     8: {
+      id: 8,
+      title: "Chương 8",
+      hasMarkdown: true
+    },
+     9: {
+      id: 9,
+      title: "Chương 9",
+      hasMarkdown: true
+    },
+     10: {
+      id: 10,
+      title: "Chương 10",
+      hasMarkdown: true
+    },
+     11: {
+      id: 11,
+      title: "Chương 11",
+      hasMarkdown: true
+    },
+     12: {
+      id: 12,
+      title: "Chương 12",
+      hasMarkdown: true
+    },
+     13: {
+      id: 13,
+      title: "Chương 13",
+      hasMarkdown: true
     }
   }
 };
